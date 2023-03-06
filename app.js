37c5cda5926a326810ae7974398ffcaece581057
krzysztoffila <umowy.fila@gmail.com>
2023-03-06 23:30:05 +0100

    Sredniki

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const btn = document.querySelector('button');
-const input = document.querySelector('input')
-const list = document.querySelector('ul')
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
 
 function addGoal() {
-    const enteredValue = input.value
-    const listItem = document.createElement('li')
+    const enteredValue = input.value;
+    const listItem = document.createElement('li');
     listItem.textContent = enteredValue;
     list.appendChild(listItem);
+    input.value = '';
 }
-btn.addEventListener('click', addGoal)
+btn.addEventListener('click', addGoal);
